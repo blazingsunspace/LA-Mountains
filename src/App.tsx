3904faf3db3f3951a69a99f0eef28c346de4052a
Milorad Đuković <donmilorad87@gmail.com>
2024-01-31 01:30:40 +0100

    Added paralax on hero title;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-
 function App() {
 	return (
-		<>
-			<Home />
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
-import headerBackground from "../../assets/images/header-background.jpg";
+
+import topPart from "../../assets/images/topPart.png";
+import bottomPart from "../../assets/images/bottomPart.png";
+import snow from "../../assets/images/snow.png";
 import "./Header.scss";
 import { useTranslation } from "react-i18next";
 
@@ -23,13 +26,20 @@ const Header = () => {
 
 	return (
 		<>
-			<header className="header df aic fdc posrelative">
+			<header className="header df aic fdc posrelative parallax">
 				<img
-					className="headerBackround w100"
-					src={headerBackground}
+					className="headerBackround firstHeaderBackground w100 parallax__bg1"
+					src={topPart}
 					alt="headerBackground"
 				></img>
-				<div className="df paddings posabsolute w100 aic">
+				<h1 className="mainTitle bebasRegular hero">
+					<p className="cBlack m0 m0 firstParagrafHeader">LOSANGELES</p> <br />
+					<p className="cPrimaryDark m0 gradient-text ">MOUNTAINS</p>
+				</h1>
+				<img src={bottomPart} alt="" className="zi10 secondHeaderBackground " />
+
+				<img src={snow} alt="" className="snow parallax__dust" />
+				<div className="df paddings posabsolute w100 aic zi10">
 					<Logo />
 					<Navigation
 						colorClass={"cWhite"}
@@ -37,12 +47,11 @@ const Header = () => {
 						showLanguageSelector
 					/>
 				</div>
-				<div className="df paddings posabsolute w100 aic b0 bWhite">
+				<div className="df paddings posabsolute w100 aic b0 bWhite zi10">
 					<Logo logoBorder={true} additionalText={true} cPrimary={"cPrimary"} />
 					<Navigation
 						colorClass={"cPrimary"}
 						navigationArray={navigationObject}
-						
 					/>
 				</div>
 			</header>
